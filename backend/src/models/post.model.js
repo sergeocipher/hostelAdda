5a3ccc2001edcefd5c0316eecdef9cb34a543dd4
Somyajit <somyajitsaha621@gmail.com>
2025-10-22 10:05:10 +0530

    change post model

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,25 +1,51 @@
 import {mongoose, Schema} from "mongoose";
 
-
 const PostType = ["ORDER", "BULK_OFFER", "BORROW"];
 const PostStatus = ["OPEN", "ORDERED", "CLOSED", "CANCELLED", "EXPIRED"];
 
-const postSchema = new Schema(
+const postSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    description: String,
-    type: { type: String, enum: PostType, required: true },
-    status: { type: String, enum: PostStatus, default: "OPEN" },
-    platform: String,
-    externalUrl: String,
-    expireAt: Date,
-    maxParticipants: Number,
-    creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    // room: { type: Schema.Types.ObjectId, ref: "Room", required: true },
+    title: {
+      type: String,
+      required: true,
+    },
+    type: { 
+      type: String, 
+      enum: PostType, 
+      required: true 
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: { 
+      type: String, 
+      enum: PostStatus, 
+      default: "OPEN" 
+    },
+    expireAt: {
+      type: Date,
+      required: true,
+    },
+    minAmount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  { timestamps: { createdAt: "createdAt" } }
+  { timestamps: true }
 );
 
 const Post =  mongoose.model("Post", postSchema);
 export default Post
-
